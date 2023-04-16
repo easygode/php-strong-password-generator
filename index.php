cf0c13620caf7370baaa4bf4930512b8edf6bcc4
easygode <bimalbenvenuti@gmail.com>
2023-04-16 11:40:37 +0200

    esercizio corretto

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php
+
+require_once __DIR__.'/functions.php';
+
+session_start();
+
+$password = '';
+$password_length = isset($_GET['password-length']) ? $_GET['password-length'] : 0;
+$repeat = isset($_GET['repeat']) ? $_GET['repeat'] : 1;
+$characters = isset($_GET['characters']) ? $_GET['characters'] : [];
+
+if(!empty($password_length)){
+
+    $password = generatePassword($password_length, $repeat, $characters);
+    $_SESSION['password'] = $password;
+    header('location: ./result.php'); 
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,22 +24,60 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Password Generator</title>
-    <!-- Bootstrap  -->
+    <!-- bootstrap css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
-    <form>
-        <div class="mb-3">
-            <label for="password-lenght" class="form-label">Password Lenght</label>
-            <input type="number" class="form-control" id="password-lenght" min="8" max="24" placeholder="Enter a number between 8 and 24">
-        </div>
-        <button type="submit" class="btn btn-primary">Generate</button>
-    </form>
-</div>
-    
-
-<!-- Bootstrap js  -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+        <h1>Password Generator</h1>
+        <form action="index.php" method="GET">
+            <div class="mb-3">
+                <label for="password-length" class="form-label">Password Length</label>
+                <input type="number" value="<?php echo $password_length; ?>" name="password-length" class="form-control" id="password-length" min="8" max="24">
+            </div>
+            <hr>
+            <h3>Scegli i caratteri</h3>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="flexCheckDefault" name="characters[]" value="a">
+                <label class="form-check-label" for="flexCheckDefault">
+                    <span>Lettere Maiuscole</span>
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="flexCheckChecked" name="characters[]" value="b">
+                <label class="form-check-label" for="flexCheckChecked">
+                    <span>Lettere Minuscole</span>
+                </label>
+            </div>
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="flexCheckChecked" name="characters[]" value="c">
+                <label class="form-check-label" for="flexCheckChecked">
+                    <span>Numeri</span>
+                </label>
+            </div>
+             <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="flexCheckChecked" name="characters[]" value="d">
+                <label class="form-check-label" for="flexCheckChecked">
+                    <span>Caratteri Speciali</span>
+                </label>
+            </div>
+            <hr>
+            <h3>Ripeti caratteri</h3>
+            <div class="form-check">
+                <input class="form-check-input" type="radio" name="repeat" id="flexRadioDefault1" value="0">
+                <label class="form-check-label" for="flexRadioDefault1">
+                    <span>Non ripetere caratteri</span>
+                </label>
+            </div>
+            <div class="form-check">
+            <input class="form-check-input" type="radio" name="repeat" id="flexRadioDefault2" value="1" checked>
+            <label class="form-check-label" for="flexRadioDefault2">
+                <span>Ripeti caratteri</span>
+            </label>
+            </div>
+            <hr>
+            <button type="submit" class="btn btn-primary">Genera</button>
+        </form>
+    </div>
 </body>
 </html>
